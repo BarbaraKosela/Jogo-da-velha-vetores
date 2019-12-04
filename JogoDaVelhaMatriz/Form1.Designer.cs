7b1897139b1e49caa34360446360dff27cd327d8
barbara.kosela <barbara@myrp.com.br>
2019-12-04 13:27:47 -0300

    Add project files.

---
+++ JogoDaVelhaMatriz/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace JogoDaVelhaMatriz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.textBox1.Location = new System.Drawing.Point(17, 12);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 86);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.textBox2.Location = new System.Drawing.Point(155, 12);
+            this.textBox2.MaxLength = 1;
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 86);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.textBox3.Location = new System.Drawing.Point(293, 12);
+            this.textBox3.MaxLength = 1;
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 86);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.textBox4.Location = new System.Drawing.Point(17, 104);
+            this.textBox4.MaxLength = 1;
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(132, 86);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.textBox5.Location = new System.Drawing.Point(155, 104);
+            this.textBox5.MaxLength = 1;
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(132, 86);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.textBox6.Location = new System.Drawing.Point(293, 104);
+            this.textBox6.MaxLength = 1;
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(132, 86);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.textBox7.Location = new System.Drawing.Point(17, 196);
+            this.textBox7.MaxLength = 1;
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(132, 86);
+            this.textBox7.TabIndex = 6;
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.textBox8.Location = new System.Drawing.Point(155, 196);
+            this.textBox8.MaxLength = 1;
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(132, 86);
+            this.textBox8.TabIndex = 7;
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.textBox9.Location = new System.Drawing.Point(293, 196);
+            this.textBox9.MaxLength = 1;
+            this.textBox9.Multiline = true;
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(132, 86);
+            this.textBox9.TabIndex = 8;
+            this.textBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button1.Location = new System.Drawing.Point(293, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 75);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Limpar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 376);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ JogoDaVelhaMatriz/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaVelhaMatriz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            string[,] campos;
+            campos = new string[3, 3];
+
+            campos[0, 0] = textBox1.Text;
+            campos[0, 1] = textBox2.Text;
+            campos[0, 2] = textBox3.Text;
+
+            campos[1, 0] = textBox4.Text;
+            campos[1, 1] = textBox5.Text;
+            campos[1, 2] = textBox6.Text;
+
+            campos[2, 0] = textBox7.Text;
+            campos[2, 1] = textBox8.Text;
+            campos[2, 2] = textBox9.Text;
+
+            bool houveVencedorNaHorizontal = ProcurarHorizontal(campos);
+            bool houveVencedorNaVertical = ProcurarVertical(campos);
+            bool houveVencedorNaDiagonal = ProcurarDiagonal(campos);
+
+            if (houveVencedorNaHorizontal == true)
+                MessageBox.Show("Vitória!");
+            else if (houveVencedorNaVertical == true)
+                MessageBox.Show("Vitória!");
+            else if (houveVencedorNaDiagonal == true)
+                MessageBox.Show("Vitória");
+            else if (houveVencedorNaVertical == false)
+                MessageBox.Show("Nem todos os campos foram preenchidos");
+            else
+                MessageBox.Show("Não houve vencedor");
+
+        }
+
+        static bool ProcurarHorizontal(string[,] vetor)
+        {
+            string resultadoX = "";
+            string resultadoO = "";
+            for (int horiz = 0; horiz < 3; horiz++)
+            {
+                for (int outroHoriz = 0; outroHoriz < 3; outroHoriz++)
+                {
+                    var linhaHorizontal = vetor[horiz, outroHoriz];
+                    if (linhaHorizontal == "X")
+                    {
+                        resultadoX = linhaHorizontal + resultadoX;
+                        if (resultadoX == "XXX")
+                            return true;
+                    }
+                    else if (linhaHorizontal == "O")
+                    {
+                        resultadoO = linhaHorizontal + resultadoO;
+                        if (resultadoO == "OOO")
+                            return true;
+                    }
+                }
+                resultadoX = "";
+                resultadoO = "";
+            }
+            return false;
+        }
+        static bool ProcurarVertical(string[,] vetor)
+        {
+            string resultadoX = "";
+            string resultadoO = "";
+            for (int vert = 0; vert < 3; vert++)
+            {
+                for (int outroVert = 0; outroVert < 3; outroVert++)
+                {
+                    var linhaVertical = vetor[outroVert, vert];
+                    if (linhaVertical == "X")
+                    {
+                        resultadoX = linhaVertical + resultadoX;
+                        if (resultadoX == "XXX")
+                            return true;
+                    }
+                    else if (linhaVertical == "O")
+                    {
+                        resultadoO = linhaVertical + resultadoO;
+                        if (resultadoO == "OOO")
+                            return true;
+                    }
+                    else if(linhaVertical == "")
+                        return false;
+                }
+                resultadoX = "";
+                resultadoO = "";
+            }
+            return false;
+        }
+        static bool ProcurarDiagonal(string[,] vetor)
+        {
+            bool haVencedorDiagonal = false;
+            while (!haVencedorDiagonal)
+            {
+                if (vetor[0, 0] == "X" && vetor[1, 1] == "X" && vetor[2, 2] == "X" ||
+                    vetor[0, 0] == "O" && vetor[1, 1] == "O" && vetor[2, 2] == "O")
+                    haVencedorDiagonal = true;
+                else if (vetor[0, 2] == "X" && vetor[1, 1] == "X" && vetor[2, 0] == "X" ||
+                    vetor[0, 2] == "O" && vetor[1, 1] == "O" && vetor[2, 0] == "O")
+                    haVencedorDiagonal = true;
+                else
+                    return false;
+            }
+            return haVencedorDiagonal;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            textBox8.Clear();
+            textBox9.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
